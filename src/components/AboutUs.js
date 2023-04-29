b2e9b3af6f4d1daa5db9103bb37a6e6402d6a27a
qoterwoter <marderics@gmail.com>
2023-04-30 01:50:53 +0300

    About Us designs

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -13,7 +13,7 @@ const AboutUs = () => {
         <main className="main">
             <div className='aboutUs'>
                 <div className="aboutUs__article">
-                    <img className='about-us__image' src='https://sun9-53.userapi.com/impg/5YnUZz9nofvilwoyUVWRo9fDOhKAsDIPsI3irQ/QKhHR-XvWyk.jpg?size=2560x1000&quality=96&sign=cb62bef7a8893f0862a4bd8758e4e12a&type=album'/>
+                    <img className='about-us__image about-us__image_left' src='https://sun9-53.userapi.com/impg/5YnUZz9nofvilwoyUVWRo9fDOhKAsDIPsI3irQ/QKhHR-XvWyk.jpg?size=2560x1000&quality=96&sign=cb62bef7a8893f0862a4bd8758e4e12a&type=album'/>
                     <p className="about-us__paragraph paragraph">
                         Эта студия звукозаписи - идеальное место для музыкантов, которые ищут качественную и
                         профессиональную запись своей музыки. Здесь можно заказать не только стандартные услуги
@@ -28,8 +28,10 @@ const AboutUs = () => {
                         воплотить в жизнь самые смелые музыкальные идеи. Все процессы звукозаписи проводятся на
                         высококлассном оборудовании, что гарантирует идеальное качество звучания.
                     </p>
-                    <img className='about-us__image' src='https://sun9-67.userapi.com/impg/pj52lK3e-OYOl1wox7N899Nj6O_UTJfzK3blzQ/3qPLYVP7Ebs.jpg?size=2560x1000&quality=96&sign=fecd3d5b43160b30ba76250984e13577&type=album'/>
-
+                    <img className='about-us__image about-us__image_right' src='https://sun9-67.userapi.com/impg/pj52lK3e-OYOl1wox7N899Nj6O_UTJfzK3blzQ/3qPLYVP7Ebs.jpg?size=2560x1000&quality=96&sign=fecd3d5b43160b30ba76250984e13577&type=album'/>
+                    {/*<div className="about-us__image"*/}
+                    {/*     style={{backgroundImage: `url('https://sun9-67.userapi.com/impg/pj52lK3e-OYOl1wox7N899Nj6O_UTJfzK3blzQ/3qPLYVP7Ebs.jpg?size=2560x1000&quality=96&sign=fecd3d5b43160b30ba76250984e13577&type=album')`}}*/}
+                    {/*/>*/}
                 </div>
             </div>
         </main>
