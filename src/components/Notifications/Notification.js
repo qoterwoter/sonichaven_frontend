88372d31a86b9c44d996a202d69fc19e6f4d9840
qoterwoter <marderics@gmail.com>
2023-05-02 19:55:30 +0300

    Close notification button

--- src/components/Notifications/Notification.js
+++ src/components/Notifications/Notification.js
@@ -1,10 +1,14 @@
 import React, {useEffect, useState} from "react";
 import {NavLink} from "react-router-dom";
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import {useDispatch} from "react-redux";
+import {removeNotification} from "../../reducers/notificationSlice";
 const Notification = (props) => {
     const state = props.notification
 
     const [classList, setClassList] = useState('show')
+
+    const dispatch = useDispatch()
 
     useEffect(() => {
         setTimeout(() => {
@@ -13,10 +17,14 @@ const Notification = (props) => {
             2500)
     })
 
+    const handleClose = () => {
+        dispatch(removeNotification())
+    }
+
     return (
         <>
             <div className={'notification notification_'+classList}>
-                <h2 className="notification__title">{state.title} <CloseRoundedIcon className='notification__close'/></h2>
+                <h2 className="notification__title">{state.title} <CloseRoundedIcon onClick={handleClose} className='notification__close'/></h2>
                 <p className="notification__description">{state.description}</p>
                 <NavLink to={state.action.url}>{state.action.button}</NavLink>
             </div>
